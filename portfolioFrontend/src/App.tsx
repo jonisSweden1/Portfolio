b46d0e447cdbdb5578411e11f16f84fff8e3ee31
jonisSweden1 <118064643+jonisSweden1@users.noreply.github.com>
2026-09-15 16:20:48 +0200

    Have made it work with bootstrap

--- portfolioFrontend/src/App.tsx
+++ portfolioFrontend/src/App.tsx
@@ -1,18 +1,25 @@
 import "./App.css"
 import logo from './Logo_portfolio.png'
+import { Nav, NavItem, NavbarBrand, Navbar, NavLink } from "react-bootstrap"
 
 export const App = () => (
   <div className="App">
-    <div>
-      <header >
-        <img src={logo} alt='logo'/>
-        <nav>
-          
-        </nav>
-      </header>
-      <section>
+      <header style={{backgroundColor: 'gray'}}>
+        <Navbar style={{display: 'flex', flexDirection: 'row'}}>
+          <NavbarBrand href="#">
+            <img src={logo} style={{backgroundColor: 'lightgray'}}/>
+          </NavbarBrand>
 
-      </section>
-    </div>
+          <Nav style={{display: 'flex', flexDirection: 'row', gap: '10px'}}>
+            <NavItem >
+              <NavLink>About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>DD</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+        
+      </header>
   </div>
 )
