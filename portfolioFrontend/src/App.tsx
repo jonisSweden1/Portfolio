ba762e0b37b8129aa83691c881ae9b4e95f04675
jonisSweden1 <118064643+jonisSweden1@users.noreply.github.com>
2026-09-15 11:40:28 +0200

    Trying to get the website to work. TODO: Need to learn basics

--- portfolioFrontend/src/App.tsx
+++ portfolioFrontend/src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css"
+import logo from './Logo_portfolio.png'
 
 export const App = () => (
   <div className="App">
     <div>
-      
+      <header >
+        <img src={logo} alt='logo'/>
+        <nav>
+          
+        </nav>
+      </header>
+      <section>
+
+      </section>
     </div>
   </div>
 )
