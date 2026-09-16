e8b2202c889e4c66cd1d2d71b406393309428a31
jonisSweden1 <118064643+jonisSweden1@users.noreply.github.com>
2026-09-16 16:15:17 +0200

    Changed name of the repository

--- portfolioFrontend/vite.config.ts
+++ portfolioFrontend/vite.config.ts
@@ -6,7 +6,7 @@ import packageJson from "./package.json" with { type: "json" }
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio",
+  base: "/jonis-portfolio",
 
   server: {
     open: true,
