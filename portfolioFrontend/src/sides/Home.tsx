fbb79b3270a9fe0712b19d3e3ed12cb5da91d94a
jonisSweden1 <118064643+jonisSweden1@users.noreply.github.com>
2026-09-17 09:24:11 +0200

    Added project side in Home.tsx

--- portfolioFrontend/src/sides/Home.tsx
+++ portfolioFrontend/src/sides/Home.tsx
@@ -1,12 +1,83 @@
 import "./Home.css"
 import background from '../Soothing-nature-backgrounds-2.jpg'
 import profile from '../cropped_circle_image.png'
+import highCommandProjectPicture from '../Jah3l_.png'
+import { Button, Card, CardBody, CardHeader, CardImg, CardText, CardTitle } from "react-bootstrap"
 
 export const HomePage = () => (
     <div id="background" style={{backgroundImage: `url(${background})`}}>
-      <section style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: '70px'}}>
-        <img src={profile} width={545}/>
-        <p style={{fontSize: '57px', color: 'white', fontFamily: ''}}>Game Developer, Software Engineer</p>
+      <section id="main-section">
+        <img src={profile}/>
+        <p>Game Developer, Software Engineer</p>
       </section>
+
+      <div id="gradient-transition"/>
+
+      <div id="project-section">
+        <h1>Projects</h1>
+        <h2>With recent ones</h2>
+
+        <div className="project-wrapper">
+        <Card style={{width: '18rem'}}>
+          <CardImg variant="top" src={highCommandProjectPicture}/>
+          <CardBody>
+            <CardTitle>High Command</CardTitle>
+            <CardText>High Command is a game made in LBS Kreativa Gymnasiet Borås. This game was nominated for best graphics in LBS Awards 2023.</CardText>
+          </CardBody>
+          <Button variant="primary" href="https://perception-studios.itch.io/high-command">Go to project</Button>
+        </Card>
+        <Card style={{width: '18rem'}}>
+          <CardHeader>
+            Project
+          </CardHeader>
+          <CardBody>
+            <CardTitle>Project Name</CardTitle>
+            <CardText>This is a project, with description about the project itself, whatever.</CardText>
+          </CardBody>
+          <Button variant="primary">Go to project</Button>
+        </Card>
+        <Card style={{width: '18rem'}}>
+          <CardHeader>
+            Project
+          </CardHeader>
+          <CardBody>
+            <CardTitle>Project Name</CardTitle>
+            <CardText>This is a project, with description about the project itself, whatever.</CardText>
+          </CardBody>
+          <Button variant="primary">Go to project</Button>
+        </Card>
+        <Card style={{width: '18rem'}}>
+          <CardHeader>
+            Project
+          </CardHeader>
+          <CardBody>
+            <CardTitle>Project Name</CardTitle>
+            <CardText>This is a project, with description about the project itself, whatever.</CardText>
+          </CardBody>
+          <Button variant="primary">Go to project</Button>
+        </Card>
+        <Card style={{width: '18rem'}}>
+          <CardHeader>
+            Project
+          </CardHeader>
+          <CardBody>
+            <CardTitle>Project Name</CardTitle>
+            <CardText>This is a project, with description about the project itself, whatever.</CardText>
+          </CardBody>
+          <Button variant="primary">Go to project</Button>
+        </Card>
+        <Card style={{width: '18rem'}}>
+          <CardHeader>
+            Project
+          </CardHeader>
+          <CardBody>
+            <CardTitle>Project Name</CardTitle>
+            <CardText>This is a project, with description about the project itself, whatever.</CardText>
+          </CardBody>
+          <Button variant="primary">Go to project</Button>
+        </Card>
+        
+        </div>
       </div>
+    </div>
 )
